2ad37d0b45d748cc09f9e85300c913eb8864a94e
viacheslavkosh <koshelevskiy.v@gmail.com>
2024-05-15 14:14:38 +0300

    first done

--- create_db.py
+++ create_db.py
@@ -28,3 +28,28 @@ cats_data = [
 
 # Вставка документів у колекцію
 cats_collection.insert_many(cats_data)
+
+
+# Виведення всіх записів із колекції
+def read_all_cats():
+    return list(cats_collection.find({}))
+
+# Виведення інформації про кота за ім'ям
+def read_cat_by_name(cat_name):
+    return cats_collection.find_one({"name": cat_name})
+
+# Оновити вік кота за ім'ям
+def update_cat_age(cat_name, new_age):
+    cats_collection.update_one({"name": cat_name}, {"$set": {"age": new_age}})
+
+# Додати нову характеристику до списку features кота за ім'ям
+def add_cat_feature(cat_name, new_feature):
+    cats_collection.update_one({"name": cat_name}, {"$push": {"features": new_feature}})
+
+# Видалення запису з колекції за ім'ям
+def delete_cat_by_name(cat_name):
+    cats_collection.delete_one({"name": cat_name})
+
+# Видалення всіх записів із колекції
+def delete_all_cats():
+    cats_collection.delete_many({})
